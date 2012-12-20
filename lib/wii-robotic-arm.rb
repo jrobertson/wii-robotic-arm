e56811be8ee3d390739d2d885aaab197d91bbd9d
Your Name <james@r0bertson.co.uk>
2012-12-20 17:02:15 +0000

    1st commit

---
+++ lib/wii-robotic-arm.rb
@@ -0,0 +1,93 @@
+#!/usr/bin/env ruby
+
+# file: wii-robotic-arm.rb
+
+require 'robotic-arm' 
+require 'simple_wiimote'
+
+class WiiRoboticArm < SimpleWiimote
+
+  def initialize()
+    super()
+    @ra = RoboticArm.new
+  end
+
+  private
+
+  def on_btn_plus_press(wm)   @ra.led.on        end
+  def on_btn_minus_press(wm)  @ra.led.off       end
+  def on_btn_plus_press(wm)   @ra.led.on        end
+  def on_btn_minus_press(wm)  @ra.led.off       end
+  def on_btn_left_down(wm)    @ra.gripper.open  end
+  def on_btn_left_up(wm)      @ra.gripper.stop  end
+  def on_btn_right_down(wm)   @ra.gripper.close end
+  def on_btn_right_up(wm)     @ra.gripper.stop  end
+
+  def on_btn_up_down(wm)
+    if @ra.wrist.moving? != :up then
+      @ra.wrist.up 
+    end
+  end
+
+  def on_btn_up_up(wm)  @ra.wrist.stop end
+
+  def on_btn_down_down(wm)
+    if @ra.wrist.moving? != :down then
+      @ra.wrist.down 
+    end
+  end
+
+  def on_btn_down_up(wm)  @ra.wrist.stop end
+
+  def on_btn_a_down(wm)
+
+    x,y = wm.acc
+
+    if x < 110 and @ra.base.moving? != :left then
+      @ra.base.left
+    elsif x > 130 and @ra.base.moving? != :right then
+      @ra.base.right
+    end
+
+    if y < 117 and @ra.shoulder.moving? != :up then
+      @ra.shoulder.up
+    elsif y > 127 and @ra.shoulder.moving? != :down then
+      @ra.shoulder.down
+    end
+
+  end
+
+  def on_btn_a_up(wm)
+    @ra.base.stop
+    @ra.shoulder.stop
+  end
+
+  def on_btn_b_down(wm)
+
+    x,y = wm.acc
+
+    if x < 110 and @ra.base.moving? != :left then
+      @ra.base.left
+    elsif x > 130 and @ra.base.moving? != :right then
+      @ra.base.right
+    end
+
+    if y < 117 and @ra.elbow.moving? != :up then
+      @ra.elbow.up
+    elsif y > 127 and @ra.elbow.moving? != :down then
+      @ra.elbow.down
+    end
+
+  end
+
+  def on_btn_b_up(wm)
+    @ra.base.stop
+    @ra.elbow.stop
+  end
+
+end
+
+if __FILE__ == $0 then
+  wra = WiiRoboticArm.new
+  wra.activate
+end
---
+++ wii-robotic-arm.gemspec
@@ -0,0 +1,9 @@
+Gem::Specification.new do |s|
+  s.name = 'wii-robotic-arm'
+  s.version = '0.1.0'
+  s.summary = 'wii-robotic-arm'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/**/*.rb']
+  s.add_dependency('simple_wiimote')
+  s.add_dependency('robotic-arm')
+end
